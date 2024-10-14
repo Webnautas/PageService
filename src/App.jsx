7128641893409a84e84e741b86f9f9c5d85db3ce
WallingtonAB <94567644+WallingtonAB@users.noreply.github.com>
2024-10-14 19:27:03 -0300

    Icon+title+teste
    é isso

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="tela inicial"></div>;
+  return <div className="tela inicial"> Jorj bem jor</div>;
 }
 
 export default App;
