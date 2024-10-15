87ae80fc900c9aee5abf607c4c5936205af0de9b
WallingtonAB <94567644+WallingtonAB@users.noreply.github.com>
2024-10-14 21:00:19 -0300

    telacadastro2

--- src/Pages/Cadastro.jsx
+++ src/Pages/Cadastro.jsx
@@ -2,5 +2,5 @@ import { useState } from "react";
 import "./Cadastro.css";
 
 export function Cadastro() {
-  return <div className="telainicial"> Jorj bem jor andrew du grau</div>;
+  return <div className="telainicial"> Jorj bem jor andrew du grau brunin cabeça gravida</div>;
 }
