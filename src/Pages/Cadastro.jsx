dcb3d551765e06a6456b2a09e36a276c78e23e1f
WallingtonAB <94567644+WallingtonAB@users.noreply.github.com>
2024-10-16 01:55:20 -0300

    TelaLoginQuaseFinalizada

--- src/Pages/Cadastro.jsx
+++ src/Pages/Cadastro.jsx
@@ -2,30 +2,29 @@ import { useState } from "react";
 import "./Cadastro.css";
 
 export function Cadastro() {
- 
   return (
     <div className="background">
       <div className="background-login">
         <h2 className="titulo">Create Account</h2>
         <div className="text-inputs">
           <div className="input-field">
-            <label className="label-text">Nome</label>
-            <input className="input-text" type="text" placeholder="Nome Completo" />
+            <label htmlFor="nome" className="label-text">Nome</label>
+            <input id="nome" className="input-text" type="text" placeholder="Nome Completo" />
           </div>
 
           <div className="input-field">
-            <label className="label-text">CPF</label>
-            <input className="input-text" type="text" placeholder="Digite seu CPF" />
+            <label htmlFor="cpf" className="label-text">CPF</label>
+            <input id="cpf" className="input-text" type="text" placeholder="Digite seu CPF" />
           </div>
 
           <div className="input-field">
-            <label className="label-text">Senha</label>
-            <input className="input-text" type="password" placeholder="Digite sua senha" />
+            <label htmlFor="senha" className="label-text">Senha</label>
+            <input id="senha" className="input-text" type="password" placeholder="Digite sua senha" />
           </div>
 
           <div className="input-field">
-            <label className="label-text">Confirmar Senha</label>
-            <input className="input-text" type="password" placeholder="Confirme sua senha" />
+            <label htmlFor="confirmarSenha" className="label-text">Confirmar Senha</label>
+            <input id="confirmarSenha" className="input-text" type="password" placeholder="Confirme sua senha" />
           </div>
         </div>
 
@@ -35,4 +34,4 @@ export function Cadastro() {
       </div>
     </div>
   );
-};
+}
