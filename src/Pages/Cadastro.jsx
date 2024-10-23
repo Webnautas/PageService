f578293b90dec0eed3c0afac9594824a52f691d5
Paulo Nunes <paulo.hnrnunes@gmail.com>
2024-10-23 17:53:18 -0300

    Alterando arquivos para comunicacao com api

--- src/Pages/Cadastro.jsx
+++ src/Pages/Cadastro.jsx
@@ -2,6 +2,41 @@ import { useState } from "react";
 import "./Cadastro.css";
 
 export function Cadastro() {
+  const [nome, setNome] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmarSenha, setConfirmarSenha] = useState("");
+
+  const handleCadastro = async () => {
+    if (senha !== confirmarSenha) {
+      alert("As senhas não conferem!");
+      return;
+    }
+
+    const cliente = { nome, cpf, senha };
+
+    try {
+      const response = await fetch("http://localhost:8081/clientes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(cliente),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        alert("Cadastro realizado com sucesso!");
+        console.log(data);
+      } else {
+        const errorData = await response.json();
+        alert("Erro ao cadastrar: " + errorData.erro);
+      }
+    } catch (error) {
+      console.error("Erro na requisição:", error);
+      alert("Erro ao conectar com a API.");
+    }
+  };
   return (
     <div className="background">
       <div className="background-login">
@@ -16,6 +51,8 @@ export function Cadastro() {
               className="input-text"
               type="text"
               placeholder="Nome Completo"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
             />
           </div>
 
@@ -28,6 +65,8 @@ export function Cadastro() {
               className="input-text"
               type="text"
               placeholder="Digite seu CPF"
+              value={cpf}
+              onChange={(e) => setCpf(e.target.value)}
             />
           </div>
 
@@ -40,6 +79,8 @@ export function Cadastro() {
               className="input-text"
               type="password"
               placeholder="Digite sua senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
             />
           </div>
 
@@ -52,12 +93,16 @@ export function Cadastro() {
               className="input-text"
               type="password"
               placeholder="Confirme sua senha"
+              value={confirmarSenha}
+              onChange={(e) => setConfirmarSenha(e.target.value)}
             />
           </div>
         </div>
 
         <div className="cadastrar">
-          <button className="cadastrar-button">CADASTRAR</button>
+          <button className="cadastrar-button" onClick={handleCadastro}>
+            CADASTRAR
+          </button>
         </div>
       </div>
     </div>
