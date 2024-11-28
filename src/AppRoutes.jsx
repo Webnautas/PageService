8c386ae0d7062cb0c4fa2af8e7bd8eacdbb75997
c0lllt <paulo.hnrnunes@gmail.com>
2024-11-28 15:03:53 -0300

    Commit final, catalogo funcinando, CRUD produtos funcionando. Tudo certo

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -3,15 +3,17 @@ import { Catalogo } from "./Pages/Catalogo";
 import { Cadastro } from "./Pages/Cadastro";
 import { Login } from "./Pages/Login";
 import { Pedido } from "./Pages/Pedido";
+import { CadastroProduto } from "./Pages/CadastroProduto";
 
 export function AppRoutes() {
   return (
     <Router>
-     <Routes>
+      <Routes>
         <Route path="/catalogo" element={<Catalogo />} />
         <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/login" element={<Login />} /> 
+        <Route path="/login" element={<Login />} />
         <Route path="/pedido" element={<Pedido />} />
+        <Route path="/cadastroproduto" element={<CadastroProduto />} />
       </Routes>
     </Router>
   );
