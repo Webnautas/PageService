db2127136d6f8da9520188b23c0bd80ab5da0f79
Andrew Izaki <Andrewtsuyoshizaki@gmail.com>
2024-11-28 17:48:44 -0300

    aa

--- src/Componentes/data.jsx
+++ src/Componentes/data.jsx
@@ -5,34 +5,4 @@ export const pizza = [
     description: " é calabresa",
     price: 40.0,
   },
-  {
-    id: 1,
-    name: "4 queijos",
-    description: " queijo",
-    price: 40.0,
-  },
-  {
-    id: 2,
-    name: "Abacaxi",
-    description: " Tem abacaxi",
-    price: 40.0,
-  },
-  {
-    id: 3,
-    name: "Chocobo",
-    description: "chocolate de brigadeiro",
-    price: 40.0,
-  },
-  {
-    id: 4,
-    name: "Banana Nevada",
-    description: "Banana, coco e leite condensado",
-    price: 40.0,
-  },
-  {
-    id: 5,
-    name: "Frango philadelfia 4 queijo",
-    description: "É frango e queijo",
-    price: 40.0,
-  },
 ];
