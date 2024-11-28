9ecb08c9a2bb8afbac46f7f77ab87672c5901f83
c0lllt <paulo.hnrnunes@gmail.com>
2024-11-28 15:57:39 -0300

    ALTERANDO ABERTURA DE PAGINA INICIAL

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: '/login', // Redireciona para /login ao iniciar o servidor
+  },
 })
